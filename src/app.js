1ae20cc70691406f306ad6f8dee11fec53d3c6c0
pa-rvez2023 <your.email@example.com>
2026-04-22 21:17:02 +0530

    Update backend for Render deployment

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://resume-ai-backend-dh0x.onrender.com",
     credentials: true
 }))
 
