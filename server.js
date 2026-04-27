22eb4a7846d36a35e09596d2d6500fe5c6d186eb
pa-rvez2023 <your.email@example.com>
2026-04-28 01:38:54 +0530

    update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const dns = require("node:dns");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 require("dotenv").config()                                        // so this .config method helps to access the data of env files in all over the express server 
+console.log("NODE_ENV:", process.env.NODE_ENV)
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 connectToDB()
